9d357c77a63079c367952969a42194b36d7fa8ed
DorianContal <dorian.contal@epitech.eu>
2023-12-15 09:42:35 +0100

    Adding Pop-up Pop-up appear when clicking and link with the case

--- src/app/Components/Calendar.tsx
+++ src/app/Components/Calendar.tsx
@@ -1,14 +1,37 @@
 "use client"
 import React, { useState } from 'react';
 
-function Calendar() {
+interface DayProps {
+  dayNumber: number;
+  isOpen: boolean;
+  onDayClick: () => void;
+}
+
+const Day: React.FC<DayProps> = ({ dayNumber, isOpen, onDayClick }) => {
+  return (
+    <div
+      onClick={onDayClick}
+      className={`w-40 h-40 border border-green-300 flex items-center justify-center rounded-md m-2 cursor-pointer 
+      ${isOpen ? 'bg-red-500 text-white' : 'bg-gray-100 text-gray-700'}`}
+    >
+      {dayNumber}
+    </div>
+  );
+};
+
+const Calendar: React.FC = () => {
   const [openedDays, setOpenedDays] = useState<number[]>([]);
+  const [selectedDay, setSelectedDay] = useState<number | null>(null);
 
   const handleDayClick = (day: number) => {
-    // Ouvrir une journée et maj
     if (!openedDays.includes(day)) {
       setOpenedDays([...openedDays, day]);
+      setSelectedDay(day);
     }
+  };
+
+  const handleClosePopup = () => {
+    setSelectedDay(null);
   };
 
   const days: number[] = Array.from({ length: 25 }, (_, index) => index + 1);
@@ -16,17 +39,24 @@ function Calendar() {
   return (
     <div className="flex flex-wrap justify-center">
       {days.map((day) => (
-        <div
+        <Day
           key={day}
-          onClick={() => handleDayClick(day)}
-          className={`w-40 h-40 border border-green-300 flex items-center justify-center rounded-md m-2 cursor-pointer 
-          ${openedDays.includes(day) ? 'bg-red-500 text-white' : 'bg-gray-100 text-gray-700'}`}
-        >
-          {day}
-        </div>
+          dayNumber={day}
+          isOpen={openedDays.includes(day)}
+          onDayClick={() => handleDayClick(day)}
+        />
       ))}
+
+      {selectedDay !== null && (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded">
+            <p>Contenu pour le jour {selectedDay}</p>
+            <button onClick={handleClosePopup}>Fermer</button>
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default Calendar;
