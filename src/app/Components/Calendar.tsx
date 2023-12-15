9db85e31a32d6e4eb3a9766ec8c615c34035b172
DorianContal <dorian.contal@epitech.eu>
2023-12-15 09:48:11 +0100

    Separation Calendar and Day Component

--- src/app/Components/Calendar.tsx
+++ src/app/Components/Calendar.tsx
@@ -1,23 +1,6 @@
 "use client"
 import React, { useState } from 'react';
-
-interface DayProps {
-  dayNumber: number;
-  isOpen: boolean;
-  onDayClick: () => void;
-}
-
-const Day: React.FC<DayProps> = ({ dayNumber, isOpen, onDayClick }) => {
-  return (
-    <div
-      onClick={onDayClick}
-      className={`w-40 h-40 border border-green-300 flex items-center justify-center rounded-md m-2 cursor-pointer 
-      ${isOpen ? 'bg-red-500 text-white' : 'bg-gray-100 text-gray-700'}`}
-    >
-      {dayNumber}
-    </div>
-  );
-};
+import Day from './Day';
 
 const Calendar: React.FC = () => {
   const [openedDays, setOpenedDays] = useState<number[]>([]);
--- src/app/Components/Day.tsx
+++ src/app/Components/Day.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-function Day() {
-  return (
-    <div>Day</div>
-  )
+interface DayProps {
+  dayNumber: number;
+  isOpen: boolean;
+  onDayClick: () => void;
 }
 
-export default Day
+const Day: React.FC<DayProps> = ({ dayNumber, isOpen, onDayClick }) => {
+  return (
+    <div
+      onClick={onDayClick}
+      className={`w-40 h-40 border border-green-300 flex items-center justify-center rounded-md m-2 cursor-pointer 
+      ${isOpen ? 'bg-red-500 text-white' : 'bg-gray-100 text-gray-700'}`}
+    >
+      {dayNumber}
+    </div>
+  );
+};
+
+export default Day;
